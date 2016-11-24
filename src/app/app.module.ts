ead5bee56c6bb60cc8f2374e414aafe12c3bff71
Canidius <ego-nav@yandex.ru>
2016-11-24 22:09:48 +0300

    #2 (feat) add components to project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,24 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { CategoryComponent } from './category/category.component';
+import { ProductComponent } from './product/product.component';
+import { SearchComponent } from './search/search.component';
+import { CheckoutComponent } from './checkout/checkout.component';
+import { ConfirmationComponent } from './confirmation/confirmation.component';
+import { MainComponent } from './main/main.component';
+import { BasketComponent } from './basket/basket.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CategoryComponent,
+    ProductComponent,
+    SearchComponent,
+    CheckoutComponent,
+    ConfirmationComponent,
+    MainComponent,
+    BasketComponent
   ],
   imports: [
     BrowserModule,
