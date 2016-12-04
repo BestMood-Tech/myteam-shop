420b5ac8fbd3a977035bb71de0fdf1f965126f21
oleg <olegskvortsovwork@mail.ru>
2016-12-04 22:02:33 +0300

    Closes #13 (feat) Сделал все согласно таску

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,17 +1,12 @@
-import { Component, OnInit } from '@angular/core';
-import { AddressFormComponent } from '../shared/components/address-form/address-form.component';
-import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
+import { Component,  } from '@angular/core';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent {
 
   constructor() {}
-
-  ngOnInit() {
-  }
 
 }
--- src/app/shared/components/product-card.component.ts
+++ src/app/shared/components/product-card.component.ts
@@ -1,5 +1,6 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { Cart } from '../services/cart.service';
+import { Auth } from '../services/auth.service';
 
 @Component({
   selector: 'app-product-card',
@@ -10,7 +11,14 @@ export class ProductCardComponent {
 
   @Input() product;
 
-  constructor(private _cart: Cart) { }
+  public productCurrency: any;
+
+  constructor(private _cart: Cart, private _auth: Auth) {}
+
+  ngOnInit() {
+    if(this._auth.user == null) this.productCurrency = "$";
+    else this.productCurrency = this._auth.user.currency;
+  }
 
   addToCart(product) {
     this._cart.addToCart(product);
