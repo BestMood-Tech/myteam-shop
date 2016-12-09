671b160d6d7484b72f84c38f73e3cb3eca953441
Andrew Zaikin <zaikin.andry@gmail.com>
2016-12-09 11:22:07 +0300

    closes #9 fix search params in url, optimize observables.

--- src/app/search/search.component.ts
+++ src/app/search/search.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl } from '@angular/forms';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { MovieService } from '../shared/services/movie.service';
 import { GamesService } from '../shared/services/games.service';
 import { MusicService } from '../shared/services/music.service';
@@ -13,54 +13,39 @@ import { Observable } from 'rxjs/Rx';
   styleUrls: ['./search.component.scss']
 })
 export class SearchComponent implements OnInit {
-
-
-  term = new FormControl();
+  public term: FormControl;
   products = [];
 
   constructor(private _musicService: MusicService,
               private _gamesService: GamesService,
               private _movieService: MovieService,
-              private _router: ActivatedRoute) {
+              private _router: Router,
+              private _route: ActivatedRoute) {
   }
 
   ngOnInit() {
-    this.term.setValue(this._router.snapshot.params['q']);
-    this._router.params
-      .filter(term => term['q'] && term['q'].length > 3)
+    this.term = new FormControl(this._route.snapshot.params['q']);
+    this._route.params
       .switchMap(term => {
         return Observable.forkJoin([
           this._musicService.search(term['q'], { limit: '10' }),
           this._gamesService.search(term['q'], { limit: '10' }),
           this._movieService.search(term['q'], { limit: '10' })
         ]);
-    })
+      })
       .subscribe(items => {
-        console.log(items);
-        this.products = [];
-        this.products = this.products.concat(this._musicService.processData(items[0]),
+        this.products = [].concat(
+          this._musicService.processData(items[0]),
           this._gamesService.processData(items[1]),
           this._movieService.processData(items[2]));
-        console.log(this.products);
       });
+
     this.term.valueChanges
       .debounceTime(500)
       .filter(item => item && item.length > 3)
       .distinctUntilChanged()
-      .mergeMap(term => {
-        return Observable.forkJoin([
-            this._musicService.search(term, { limit: '10' }),
-            this._gamesService.search(term, { limit: '10' }),
-            this._movieService.search(term, { limit: '10' })
-          ]);
-      })
-      .subscribe(items => {
-        console.log(items);
-        this.products = [];
-        this.products = this.products.concat(this._musicService.processData(items[0]),
-          this._gamesService.processData(items[1]),
-          this._movieService.processData(items[2]));
-        console.log(this.products);
+      .subscribe(term => {
+        this._router.navigate(['/search', { q: term }])
       });
   };
 }
