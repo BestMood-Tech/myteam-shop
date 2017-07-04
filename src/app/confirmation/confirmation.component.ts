d1a349727b63b6e6ec5689980806532d1efa6f69
sermolenko-bmt <s.ermolenko@bestmood.tech>
2017-07-04 22:51:32 +0300

    fix display date in confirmation page

--- src/app/confirmation/confirmation.component.ts
+++ src/app/confirmation/confirmation.component.ts
@@ -23,7 +23,7 @@ export class ConfirmationComponent implements OnInit {
     this.orderUser = this.auth.user.lastName + ' ' + this.auth.user.firstName;
     this.order = this.auth.user.orders[this.auth.user.orders.length - 1];
     this.addressOrder = new Address(this.order.addressOrder);
-    this.orderDate.setDate(new Date(this.order.data).getDate() + 14);
+    this.orderDate.setDate(new Date(this.order.date).getDate() + 14);
     this.toastr.success('Your order has been successfully processed', 'Success!');
   }
 
