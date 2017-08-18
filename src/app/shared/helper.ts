adf5d6b0e34842b31391e82fb530811df709b796
sermolenko-bmt <s.ermolenko@bestmood.tech>
2017-08-18 22:26:24 +0300

    change baseUrl

--- src/app/shared/helper.ts
+++ src/app/shared/helper.ts
@@ -1,7 +1,7 @@
 import { Headers, RequestOptions } from '@angular/http';
 
-// export const baseUrl = 'https://iyr14wnsv3.execute-api.eu-central-1.amazonaws.com/dev/';
-export const baseUrl = 'http://localhost:3000/'; // for local development
+export const baseUrl = 'https://iyr14wnsv3.execute-api.eu-central-1.amazonaws.com/dev/';
+// export const baseUrl = 'http://localhost:3000/'; // for local development
 export const siteKeyGC = '6LduCCoUAAAAABhZ822fD5A_BqNQH0RJMSV_DJjn';
 
 export function setOptions() {
