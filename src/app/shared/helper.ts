5d4258c3df804f4edf34a2433ba4222fbb246e13
Andrew Zaikin <zaikin.andry@gmail.com>
2018-04-12 12:31:09 +0300

    - env

--- src/app/shared/helper.ts
+++ src/app/shared/helper.ts
@@ -1,9 +1,9 @@
 import { Headers, RequestOptions } from '@angular/http';
 
-export const baseUrl = 'https://oq7k3diobg.execute-api.eu-central-1.amazonaws.com/dev/';
-// export const baseUrl = 'http://localhost:3000/'; // for local development
-export const bucketUrl = 'https://s3.eu-central-1.amazonaws.com/bmt-media-shop-service-pdf/';
-// export const bucketUrl = 'http://localhost:8800/bmt-media-shop-service-pdf/'; // for local development
+// export const baseUrl = 'https://oq7k3diobg.execute-api.eu-central-1.amazonaws.com/dev/';
+export const baseUrl = 'http://localhost:3000/'; // for local development
+// export const bucketUrl = 'https://s3.eu-central-1.amazonaws.com/bmt-media-shop-service-pdf/';
+export const bucketUrl = 'http://localhost:8800/bmt-media-shop-service-pdf/'; // for local development
 export const siteKeyGC = '6LduCCoUAAAAABhZ822fD5A_BqNQH0RJMSV_DJjn';
 
 export function setOptions() {
